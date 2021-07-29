874359f5514e2fbde054721be5d592eb1540b5e9
andyjzhong <andyjzhong@gmail.com>
2021-07-29 11:14:22 -0400

    add initial button event listener

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+'use-strict';
+
 console.warn("JS loaded.");
+
+const menuItemBtn = document.querySelector(".menu-item");
+
+menuItemBtn.addEventListener("click", function() {
+    console.log("Button was clicked.");
+})
