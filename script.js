90b2f616317c708efc8012a934ed189f8028dcbc
andyjzhong <andyjzhong@gmail.com>
2021-07-29 11:33:58 -0400

    provide dummy data from api

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const menuItemBtn = document.querySelector(".menu-item");
 menuItemBtn.addEventListener("click", function() {
     console.log("Button was clicked.");
 })
+
+fetch("/sampleData.json")
+.then((res) => {
+  return res.json();
+})
+.then(function(res) {
+    console.log("Retrieved data successfully.", res);
+})
+.catch(function(err) {
+    console.log("Failed to retrieve data.", err);
+})
