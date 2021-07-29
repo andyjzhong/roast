1437ae0599a5f92aa69a34907df3902a9a685744
andyjzhong <andyjzhong@gmail.com>
2021-07-29 13:34:29 -0400

    grab a muffin

--- script.js
+++ script.js
@@ -3,10 +3,7 @@
 console.warn("JS loaded.");
 
 const menuItemBtn = document.querySelector(".menu-item");
-
-menuItemBtn.addEventListener("click", function() {
-    console.log("Button was clicked.");
-})
+const activeOrder = [];
 
 fetch("/sampleData.json")
 .then((res) => {
@@ -14,7 +11,14 @@ fetch("/sampleData.json")
 })
 .then(function(res) {
     console.log("Retrieved data successfully.", res);
+    menuItemSelected = res.result.menus[0].menu_sections[0].menu_items[0]
+    console.log(menuItemSelected);
 })
 .catch(function(err) {
     console.log("Failed to retrieve data.", err);
+})
+
+// Event Handlers
+menuItemBtn.addEventListener("click", function() {
+    console.log("Button was clicked.");
 })
