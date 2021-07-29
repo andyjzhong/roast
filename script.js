a45ab80f6ff1e8d5b322dfe019e7ea8c80fb054b
andyjzhong <andyjzhong@gmail.com>
2021-07-29 13:37:53 -0400

    push selected object when button is clicked

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ fetch("/sampleData.json")
 .then(function(res) {
     console.log("Retrieved data successfully.", res);
     menuItemSelected = res.result.menus[0].menu_sections[0].menu_items[0]
-    console.log(menuItemSelected);
 })
 .catch(function(err) {
     console.log("Failed to retrieve data.", err);
@@ -21,4 +20,6 @@ fetch("/sampleData.json")
 // Event Handlers
 menuItemBtn.addEventListener("click", function() {
     console.log("Button was clicked.");
+    activeOrder.push(menuItemSelected)
+    console.log(activeOrder);
 })
